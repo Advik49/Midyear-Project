08e3dfda9d46351d60ff91fe53f52b2813f66cb7
Advik49 <advik.adhiyaman@gmail.com>
2023-03-06 02:20:55 -0500

    Semi-Finished Game

--- Midyear Project/MainMenu.gd
+++ Midyear Project/MainMenu.gd
@@ -17,7 +17,7 @@ func _ready():
 
 
 func _on_StartButton_pressed():
-	get_tree().change_scene("res://Levels/World_Scene.tscn")
+	get_tree().change_scene("res://Levels/Level_3.tscn")
 
 
 func _on_SettingsButton_pressed():
