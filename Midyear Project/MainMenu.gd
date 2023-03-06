ee02bd05d4c29d1b4d8dce6c181ecad00d7bb535
Advik49 <advik.adhiyaman@gmail.com>
2023-03-06 09:21:59 -0500

    Finished Game real

--- Midyear Project/MainMenu.gd
+++ Midyear Project/MainMenu.gd
@@ -17,10 +17,12 @@ func _ready():
 
 
 func _on_StartButton_pressed():
+# warning-ignore:return_value_discarded
 	get_tree().change_scene("res://Levels/World_Scene.tscn")
 
 
 func _on_SettingsButton_pressed():
+# warning-ignore:return_value_discarded
 	get_tree().change_scene("res://SettingsMenu.tscn")
 
 func _on_QuitButton_pressed():
