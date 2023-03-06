055128750c195bcef4ff47b5bb9e83601c405513
Advik49 <advik.adhiyaman@gmail.com>
2023-03-06 03:22:34 -0500

    Changed Title and Added PauseMenu

--- Midyear Project/ PauseMenu.gd
+++ Midyear Project/ PauseMenu.gd
@@ -4,6 +4,7 @@ extends Control
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+	
 var is_paused = false setget set_is_paused
 func _unhandled_input(event):
 	if event.is_action_pressed("pause"):
@@ -14,15 +15,9 @@ func set_is_paused(value):
 	get_tree().paused = is_paused
 	visible = is_paused
 
-func _ready():
-	pass # Replace with function body.
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
-
-
 func _on_Resume_pressed():
 	self.is_paused = false
 
